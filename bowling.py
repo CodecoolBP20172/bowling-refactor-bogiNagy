a2aca5953b09bae88a4727b7642745659a8e47df
Boglarka Nagy <thebogosz@gmail.com>
2017-07-27 17:20:02 +0200

    pep8

--- bowling.py
+++ bowling.py
@@ -9,17 +9,17 @@ def score(game):
             result += get_value(game[i])
         if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
-                result += get_value(game[i+1])
+                result += get_value(game[i + 1])
             elif game[i] in 'Xx':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+                result += get_value(game[i + 1])
+                if game[i + 2] == '/':
+                    result += 10 - get_value(game[i + 1])
                 else:
-                    result += get_value(game[i+2])
+                    result += get_value(game[i + 2])
         last = get_value(game[i])
         if not in_first_half:
             frame += 1
-        if in_first_half is True:
+        if in_first_half:
             in_first_half = False
         else:
             in_first_half = True
